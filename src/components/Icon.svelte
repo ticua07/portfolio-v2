7b4a6d3da9d34d2e06fb731d7695d818c9dbd0b2
Ticua <ticua@ticua.ar>
2024-01-11 04:01:30 -0300

    Finished page

---
+++ src/components/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let url: string;
+    export let src: string;
+    export let alt: string;
+</script>
+
+<a href={url}>
+    <img {src} srcset={src} {alt} />
+</a>
+
+<style>
+    img {
+        width: 64px;
+        height: 64px;
+        opacity: 50%;
+        transition: opacity 0.2s;
+    }
+    img:hover {
+        opacity: 1;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,96 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Icon from "../components/Icon.svelte";
+</script>
+
+<div class="container">
+    <div class="portfolio">
+        <img
+            class="image"
+            src="./cat.png"
+            alt="Character Madeline from Celeste"
+        />
+        <div class="info-container">
+            <div class="text-container">
+                <h1>Ticua</h1>
+                <h2>
+                    Passionate developer who loves
+                    <span style="color: #F0DB4F">JavaScript</span>
+                    and <span style="color: #e53617">Rust</span>.
+
+                    <br />With knowledge in both front-end and back-end services
+                </h2>
+            </div>
+            <div class="icons">
+                <Icon
+                    url="https://github.com/ticua07"
+                    alt="Github's Logo"
+                    src="gh-logo.svg"
+                />
+                <Icon
+                    url="https://discordapp.com/users/448952682611474442"
+                    alt="Discord's logo"
+                    src="ds-logo.svg"
+                />
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap");
+
+    .container {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #141414;
+    }
+
+    .portfolio {
+        display: flex;
+        flex-direction: row;
+        gap: 25px;
+    }
+    .image {
+        height: 300px;
+        width: 300px;
+    }
+    .info-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding-right: 10px;
+    }
+
+    .text-container {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .icons {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 40px;
+    }
+
+    .icon {
+        height: 54px;
+    }
+
+    h1 {
+        color: #fff;
+        font-size: 48px;
+    }
+    h2 {
+        font-size: 25px;
+        opacity: 93.33%;
+        color: #eee;
+    }
+    h1,
+    h2 {
+        font-family: "Inter";
+    }
+</style>
